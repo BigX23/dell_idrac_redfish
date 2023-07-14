8243da40e78896d13a8bf52080b0b61a83013223
mwhite@ssnc-corp.global <mwhite@ssnc-corp.global>
2023-07-14 13:53:35 -0400

    Updated docstrings

--- idrac_test.py
+++ idrac_test.py
@@ -1,9 +1,38 @@
+"""
+This script will collect iDRAC details via input from the user and then connect to the
+ edfish API endpoint of an iDRAC, query the BIOS settings, and return the BIOS data in json format.
+
+This script requires the following modules:
+    getpass
+    requests
+    pprint
+
+This file contains the following function:
+    * get_BIOS_Settings - returns the BIOS settings of the iDRAC in json format
+
+This file contains the following if __name__ == "__main__" block:
+    * Inputs the iDRAC FQDN, username, and password
+    * Prints the BIOS settings of the iDRAC in json format
+    * Prints the Service Tag of the iDRAC
+"""
 import getpass
 import requests
 from pprint import pprint
 
 
 def get_BIOS_Settings(hostname, user, cred):
+    """
+    This function will connect to the Redfish API endpoint of an iDRAC and query the BIOS settings
+    and return the BIOS data in json format.
+
+    Parameters:
+    hostname (str): The FQDN of the iDRAC
+    user (str): The username of the iDRAC
+    cred (str): The password of the iDRAC
+
+    Returns:
+    data (dict): The BIOS settings of the iDRAC in json format
+    """
     biosHead = {'content-type': 'application/json'}
     GETURL = f"https://{hostname}/redfish/v1/Systems/System.Embedded.1/Bios"
 
